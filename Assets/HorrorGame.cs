5d3cbfd62d0a31cba9c4e3a22b7b104c536f5215
Lix311 <alexsterrantino@gmail.com>
2020-11-04 16:31:26 -0500

    first commit

---
+++ Assets/HorrorGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HorrorGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text =  ("You wake up in a Hospital Bed. Where am I? What do you do?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
